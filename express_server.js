36dee078b5b10c5a7e23830387103440cb438510
Sarada <sarada@ranjits-mbp.lan>
2021-09-28 07:45:13 -0400

    implemented a basic web server using the Express.js framework

--- express_server.js
+++ express_server.js
@@ -34,3 +34,15 @@ app.get("/urls.json", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL        };
     res.render("urls_show", templateVars);
   });
+
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
+  const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
