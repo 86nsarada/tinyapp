cb82895f9ce85fdaf2bd0efe79ece8217bf53236
Sarada <sarada@ranjits-mbp.lan>
2021-09-30 18:09:58 -0400

    new url

--- express_server.js
+++ express_server.js
@@ -9,29 +9,36 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+//*************************************************** */
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
+//******************************************************* */
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
+//********************************************************* */
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
+//********************************************************* */
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
+/********************************************************* */
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: null };
+  const templateVars = { urls: urlDatabase, username: "sarada" };
   res.render("urls_index", templateVars);
 });
-
+/********************************************************* */
+app.get("/urls/new", (req, res) => {
+  const templateVars = { urls: urlDatabase, username: "sarada" };
+  res.render("urls_new",templateVars);
+});
+/********************************************************* */
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL, username: null };
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL, username: "sarada" };
   res.render("urls_show", templateVars);
 });
+
+/********************************************************* */
