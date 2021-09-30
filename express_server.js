913604c03b02cc1c51f0f163ef68e22b76368ff7
Sarada <sarada@ranjits-mbp.lan>
2021-09-29 23:42:36 -0400

    adding new data in all files

--- express_server.js
+++ express_server.js
@@ -2,14 +2,52 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const uuid = require('uuid/v4');
+const cookieParser = require('cookie-parser');
+
+// Declared objects: url and user databases to be used in routes and functions
+const urlDataBase = {
+  b2xVn2: {
+    id: 'userRandomID',
+    url: 'http://www.lighthouselabs.ca',
+    visits: 0
+  },
+  P80OsK: {
+    id: 'userRandomID',
+    url: 'https://github.com',
+    visits: 0
+  },
+  s9m5xK: {
+    id: 'user2RandomID',
+    url: 'http://www.google.com',
+    visits: 0
+  },
+  oSLt22: {
+    id: 'user2RandomID',
+    url: 'https://developer.mozilla.org',
+    visits: 0
+  }
+};
+
+const usersDb = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 app.set("view engine", "ejs");
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+// const urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
 
 const generateRandomString = function(){
     let result = '';
@@ -20,14 +58,6 @@ const generateRandomString = function(){
     return result;
 }
 
-
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
-
-// app.listen(PORT, () => {
-//   console.log(`Example app listening on port ${PORT}!`);
-// });
 
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
@@ -41,20 +71,55 @@ app.get("/urls.json", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
   });
+//..............................................................  
 
   app.get("/urls/:shortURL", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL        };
     res.render("urls_show", templateVars);
   });
+//..................................................................
 
-  app.get("/login",(req, res)=> {
-      res.render(login)
-  })
+
+  app.get('/login', (req, res) => {
+    // if we here, we take for granted that the user is not logged in.
+    const templateVars = { user: null };
+  
+    res.render('login', templateVars);
+  });
+
+  app.post('/login', (req, res) => {
+    // extract the email and password from the body of request => req.body
+  
+    const email = req.body.email;
+    const password = req.body.password;
+  
+  
+    // compare the passwords
+    // password match => log in
+    // password dont' match => error message
+  
+    const user = authenticateUser(email, password, usersDb);
+  
+    if (user) {
+      // user is authenticated
+      // setting the cookie
+      res.cookie('user_id', user.id);
+  
+      // redirect to /quotes
+      res.redirect('/urls'); //=> hey browser, can you do another request => get /quotes
+      return;
+    }
+  
+    // user is not authenticated => send error
+  
+    res.status(401).send('Wrong credentials!');
+  });
 
   app.get("/urls/new", (req, res) => {
     res.render("urls_new");
   });
 
+  //..........................................................................
 //   const bodyParser = require("body-parser");
 // app.use(bodyParser.urlencoded({extended: true}));
 
@@ -86,8 +151,8 @@ app.post('/urls/:shortURL/delete',(req, res)=>{
     }
 })
 
-  app.post("/urls/shortURL/edit",(req, res)=> {
-      //console.log(req.body.b2xVn2);
+  app.post("/urls/:shortURL/edit",(req, res)=> {
+      console.log(req.body.shortURL);
       if(urlDatabase[req.params.shortURL]){
         res.redirect('/urls')
         res.send("record id updated") 
